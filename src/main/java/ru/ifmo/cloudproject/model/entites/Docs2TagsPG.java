f342d416130afa4855267e7a33ffc5c211255c1a
kirrog <rroogg76aacchh@gmail.com>
2023-01-08 16:37:18 +0300

    feat: postgres entities

---
+++ src/main/java/ru/ifmo/cloudproject/model/entites/Docs2TagsPG.java
@@ -0,0 +1,25 @@
+package ru.ifmo.cloudproject.model.entites;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Table(name = "docs2tags")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Builder
+@AllArgsConstructor
+public class Docs2TagsPG {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "docId", referencedColumnName = "id", nullable = false)
+    private DocumentPG docId;
+    @ManyToOne
+    @JoinColumn(name = "tagId", referencedColumnName = "id", nullable = false)
+    private TagsPG tagId;
+}
---
+++ src/main/java/ru/ifmo/cloudproject/model/entites/TagsPG.java
@@ -0,0 +1,26 @@
+package ru.ifmo.cloudproject.model.entites;
+
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "tags")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Builder
+@AllArgsConstructor
+public class TagsPG {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+}
